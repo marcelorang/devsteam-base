3a241c9639f2067a8e69a54f3d6ef34fd23e2557
marcelorang <mclowebdev@gmail.com>
2023-07-10 21:21:08 -0300

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
+import Navbar from '@/components/navbar/navbar'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>DevSteam: A sua loja online de games</title>
+        <title>Steamleza: A sua loja online de games</title>
         <meta name="description" content="DevSteam: A sua loja online de games" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        DevStart: A sua loja online de games
+       <Navbar />
       </div>
     </>
   )
